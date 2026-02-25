fde927ffa96843574af735d877ec460dffe6ad3f
zakirashirli <117630695+zakirashirli@users.noreply.github.com>
2026-02-25 22:04:12 +0400

    @Pattern

--- library/src/main/java/com/library/dea/dto/RegisterForm.java
+++ library/src/main/java/com/library/dea/dto/RegisterForm.java
@@ -1,17 +1,28 @@
 package com.library.dea.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
 
 
 public class RegisterForm {
     @NotBlank
     @Size(min = 4, max = 50)
+    @Pattern(
+            regexp = "^[a-zA-Z0-9_]+$",
+            message = "Username может содержать только буквы, цифры и '_'."
+    )
     private String username;
     @NotBlank
-    @Size(min = 4, max = 50)
-    private String password;
+    @Size(min = 8, max = 50)
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*]).{8,}$",
+            message = "Password слишком слабый! Нужны цифры, разные регистры и спецсимволы."
+    )
+    private String password; // min 8 1A-Z 1
+
+    @NotBlank(message = "Подвердите пароль!")
+    private String confirmPassword;
 
     public String getUsername() {
         return username;
@@ -27,5 +38,13 @@ public class RegisterForm {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
     }
 }
--- library/src/main/java/com/library/dea/service/UserService.java
+++ library/src/main/java/com/library/dea/service/UserService.java
@@ -25,6 +25,9 @@ public class UserService {
 
         user.setUsername(form.getUsername());
         user.setPassword(passwordEncoder.encode(form.getPassword()));
+        if (!form.getPassword().equals(form.getConfirmPassword())) {
+            throw new RuntimeException("Пароли не совпадают!");
+        }
         user.setRole("ROLE_USER");
 
         userRepository.save(user);
