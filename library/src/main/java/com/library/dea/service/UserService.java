ef735edfa77687d3e406ff5992c1eab6e9868990
zakirashirli <117630695+zakirashirli@users.noreply.github.com>
2026-03-14 19:49:38 +0400

    Updated UserService

--- library/src/main/java/com/library/dea/service/UserService.java
+++ library/src/main/java/com/library/dea/service/UserService.java
@@ -1,35 +1,7 @@
 package com.library.dea.service;
 
 import com.library.dea.dto.RegisterForm;
-import com.library.dea.entity.User;
-import com.library.dea.repository.UserRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    public void register(RegisterForm form) {
-        if (userRepository.existsByUsername(form.getUsername())) {
-            throw new RuntimeException("Username already exists");
-        }
-
-        User user = new User();
-
-        user.setUsername(form.getUsername());
-        user.setPassword(passwordEncoder.encode(form.getPassword()));
-        if (!form.getPassword().equals(form.getConfirmPassword())) {
-            throw new RuntimeException("Пароли не совпадают!");
-        }
-        user.setRole("ROLE_USER");
-
-        userRepository.save(user);
-    }
+public interface UserService {
+    void register(RegisterForm form);
 }
